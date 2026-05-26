5f688c0afa51c2381736fb8ef866b88b7bdc4ddd
Thiago Costa <thiagoacnascimento@gmail.com>
2026-05-25 21:34:59 -0300

    feat: add env-config methods

--- src/shared/infrastructure/env-config/env-config.module.ts
+++ src/shared/infrastructure/env-config/env-config.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { EnvConfigService } from './env-config.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [ConfigModule],
   providers: [EnvConfigService],
 })
 export class EnvConfigModule {}
