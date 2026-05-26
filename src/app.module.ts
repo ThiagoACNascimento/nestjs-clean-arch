37e4c29dbcd53cededf4540db178a704e967c3ef
Thiago Costa <thiagoacnascimento@gmail.com>
2026-05-25 21:28:40 -0300

    feat: add env-config module on app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module';
 
 @Module({
-  imports: [],
+  imports: [EnvConfigModule],
 })
 export class AppModule {}
