f640bde4d21c7d6b3f500d53727fdadc960b99da
Thiago Costa <thiagoacnascimento@gmail.com>
2026-05-27 21:04:35 -0300

    feat: add setter, getter and method tests

--- src/users/domain/entities/test/user.entity.spec.ts
+++ src/users/domain/entities/test/user.entity.spec.ts
@@ -21,26 +21,53 @@ describe('UserEntity unit tests', () => {
     expect(sut.props.createdAt).toBeInstanceOf(Date);
   });
 
-  it('Getter of name field', () => {
-    expect(sut.props.name).toBeDefined();
-    expect(sut.props.name).toEqual(props.name);
-    expect(typeof sut.props.name).toBe('string');
+  describe('Getters', () => {
+    it('Getter of name field', () => {
+      expect(sut.props.name).toBeDefined();
+      expect(sut.props.name).toEqual(props.name);
+      expect(typeof sut.props.name).toBe('string');
+    });
+
+    it('Getter of email field', () => {
+      expect(sut.props.email).toBeDefined();
+      expect(sut.props.email).toEqual(props.email);
+      expect(typeof sut.props.email).toBe('string');
+    });
+
+    it('Getter of passwordHash field', () => {
+      expect(sut.props.passwordHash).toBeDefined();
+      expect(sut.props.passwordHash).toEqual(props.passwordHash);
+      expect(typeof sut.props.passwordHash).toBe('string');
+    });
+
+    it('Getter of createdAt field', () => {
+      expect(sut.props.createdAt).toBeDefined();
+      expect(sut.props.createdAt).toBeInstanceOf(Date);
+    });
   });
 
-  it('Getter of email field', () => {
-    expect(sut.props.email).toBeDefined();
-    expect(sut.props.email).toEqual(props.email);
-    expect(typeof sut.props.email).toBe('string');
+  describe('Setters', () => {
+    it('Name field', () => {
+      sut['name'] = 'Name2';
+      expect(sut.props.name).toEqual('Name2');
+      expect(typeof sut.props.name).toBe('string');
+    });
+
+    it('Password field', () => {
+      sut['passwordHash'] = 'password2';
+      expect(sut.props.passwordHash).toEqual('password2');
+      expect(typeof sut.props.passwordHash).toBe('string');
+    });
   });
 
-  it('Getter of passwordHash field', () => {
-    expect(sut.props.passwordHash).toBeDefined();
-    expect(sut.props.passwordHash).toEqual(props.passwordHash);
-    expect(typeof sut.props.passwordHash).toBe('string');
-  });
-
-  it('Getter of createdAt field', () => {
-    expect(sut.props.createdAt).toBeDefined();
-    expect(sut.props.createdAt).toBeInstanceOf(Date);
+  describe('Update methods', () => {
+    it('Name field', () => {
+      sut.updateName('Name3');
+      expect(sut.props.name).toEqual('Name3');
+    });
+    it('PassowordHash field', () => {
+      sut.updatePasswordHash('Password3');
+      expect(sut.props.passwordHash).toEqual('Password3');
+    });
   });
 });
